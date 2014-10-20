25cec5229503d519b2e6bca2c83066f68fb77140
Afnan Enayet <afnanenayet@users.noreply.github.com>
2014-10-20 10:01:42 -0400

    Update pollutantmean.R

--- pollutantmean.R
+++ pollutantmean.R
@@ -1,13 +1,14 @@
 pollutantmean <- function(directory, pollutant, id = 1:332) {
-  setwd(directory)
-  ##wcolumn <- [,pollutant]
+  filenames <- list.files(directory, pattern="*.csv", full.names=TRUE)
   files <- dir()
   data <- numeric()
   data<-data.frame()
   
-  for(i in id) {
-    data<-rbind(data,read.csv(files[i]))
-  }
+  for (i in id) {
+        newRead = read.csv(paste(directory, "/", 
+            ".csv", sep = ""))
+        data = c(data, newRead[[pollutant]])
+    }
   
   return(mean(data, na.rm = TRUE))
 }
