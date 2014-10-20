b87e5661ab4018fd4961694e33511a7da6079ebf
Afnan Enayet <afnanenayet@users.noreply.github.com>
2014-10-20 09:16:42 -0400

    fixed function
    lel

--- pollutantmean.R
+++ pollutantmean.R
@@ -1,2 +1,13 @@
 pollutantmean <- function(directory, pollutant, id = 1:332) {
+  setwd(directory)
+  ##wcolumn <- [,pollutant]
+  files <- dir()
+  data <- numeric()
+  data<-data.frame()
   
+  for(i in id) {
+    data<-rbind(data,read.csv(files[i]))
+  }
+  
+  return(mean(data, na.rm = TRUE))
+}
